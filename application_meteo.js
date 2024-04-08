a7d3e058d9cfdb8ee35a184dab84caffebed51bc
Sylvainxiii <sylvainlacroix@protonmail.com>
2024-04-08 18:18:48 +0200

    Refacture des fonctions d'ajout et de suppression de ville

--- application_meteo.js
+++ application_meteo.js
@@ -1,21 +1,25 @@
+// DEFINITION DES VARIABLES DE NAVIGATION DANS LE DOM-------------------------------------------------------------------------------------------------------------------------------------
+
 let container = document.getElementById('corps');
 let rechercheBtn = document.getElementById('searchButton');
-let supprimerValidation = document.getElementById('lineDeleteAction');
+let supprimerValidation = document.getElementById('modaleSuppression');
 let unite = document.getElementById('unit');
 let temperatureVilleNode = document.getElementsByClassName('tempville');
 let temperatureUnitNode = document.getElementsByClassName('tempunit');
-let inputVille = document.getElementById('rechercheville');
+let barreDeRechercheVille = document.getElementById('rechercheville');
+let deletetext = document.getElementById('supTexte');
 let confirmerSupprimer = document.getElementById('confirm');
 let supprimerAnnule = document.getElementById('cancel');
-let villeASupprimer;
-let villeIndex;
-let supprimerBtn;
-let listeVille = []; //listeVille est un array qui stocke les noms des villes présentes dans local storage + celles qui sont ajoutées via la barre de recherche
-if (localStorage.getItem('ville') !== null) { listeVille = JSON.parse(localStorage.getItem('ville')) }; // charge dans listeVille le contenu de local storage, attention localstorage est un string et listeVille un array
 let cleApi = '967a9bd33d49966284cd4709635d0491';
 
-// récupération des données dans l'API openweather map
-function afficherMeteo(nomVille, apikey, nouvelleVille = false) {
+//RECUPERATION ET AFFICHAGE DES DONNEES METEO PAR VILLE---------------------------------------------------------------------------------------------------------------------------------
+
+function createUrl(nomVille, apikey) {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${nomVille}&appid=${apikey}`;
+    return url
+}
+// récupération des données dans l'API openweather map et création de l'élement HTML contenant les données
+function afficherMeteo(nomVille, apikey) {
 
     fetch(createUrl(nomVille, apikey)).then(response => response.json())
         .then(data => {
@@ -28,11 +32,6 @@ function afficherMeteo(nomVille, apikey, nouvelleVille = false) {
 
             creationDivVille(container, nomVille, icone, temperature, humidite);
 
-            // implemente localstorage si ajout d'une nouvelle ville via le formulaire
-            if (villeDansLocalStorage(nomVille) == false) {
-                implementLocalStorage(nomVille)
-            }
-
         })
         .catch((error) => {
             if (error) {
@@ -41,29 +40,7 @@ function afficherMeteo(nomVille, apikey, nouvelleVille = false) {
         })
 }
 
-function createUrl(nomVille, apikey) {
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${nomVille}&appid=${apikey}`;
-    return url
-}
-
-function extractLocalStorage() {
-    let listeVille = []; //listeVille est un array qui stocke les noms des villes présentes dans local storage + celles qui sont ajoutées via la barre de recherche
-    if (localStorage.getItem('ville') !== null) { listeVille = JSON.parse(localStorage.getItem('ville')) }; // charge dans listeVille le contenu de local storage, attention localstorage est un string et listeVille un array    
-
-    return listeVille
-}
-
-function villeDansLocalStorage(nomVille) {
-    extractLocalStorage();
-    return listeVille.includes(nomVille);
-}
-
-function implementLocalStorage(nomVille) {
-    local = extractLocalStorage();
-    local.push(nomVille);
-    localStorage.setItem('ville', JSON.stringify(local));
-    return true
-}
+//Fonction de création de l'élément HTML contenant les données météo d'une ville
 function creationDivVille(divParent, divNomVille, divIcone, divTemperature, divHumidite) {
     nomPropreVille = divNomVille.charAt(0).toUpperCase() + divNomVille.slice(1); // Affiche le nom de la ville avec la première lettre en majuscule
     //Création d'un nouvel élément
@@ -77,84 +54,125 @@ function creationDivVille(divParent, divNomVille, divIcone, divTemperature, divH
         "<div class='humidite_texte'><p class='humidite_titre'>Humidité:</p><p class='humidite'>"
         + divHumidite + "</p></div><div class='deleteButton btn' ><img src='icon/deletewhite.svg' alt='Sup' class='deleteicon'></div>";
 
-    return
+    return true
 }
 
+//OPERATIONS DANS LE LOCAL STORAGE--------------------------------------------------------------------------------------------------------------------------------------------------
+
+function extractLocalStorage() {
+    let listeVille = []; //listeVille est un array qui stocke les noms des villes présentes dans local storage + celles qui sont ajoutées via la barre de recherche
+    if (localStorage.getItem('ville') !== null) { listeVille = JSON.parse(localStorage.getItem('ville')) }; // charge dans listeVille le contenu de local storage, attention localstorage est un string et listeVille un array    
+
+    return listeVille
+}
+
+function villeDansLocalStorage(nomVille) {
+    local = extractLocalStorage();
+    return local.includes(nomVille);
+}
+
+function implementLocalStorage(nomVille) {
+    local = extractLocalStorage();
+    local.push(nomVille);
+    localStorage.setItem('ville', JSON.stringify(local));
+    return true
+}
+
+function supprimerDeLocalStorage(nomVille) {
+    local = extractLocalStorage();
+    let villeIndex = local.indexOf(nomVille);
+    local.splice(villeIndex, 1);
+    localStorage.setItem('ville', JSON.stringify(local));
+    return true
+}
+
+// ACTIONS AU CHARGEMENT DE LA PAGE-------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Affiche les données pour chaque ville stockées dans localstorage au démarrage de l'appli
+document.onload = Chargement(extractLocalStorage(), cleApi);
 
 //charge les villes présentes dans le local storage
-function Chargement(listeVille) {
-    if (listeVille !== null) {
+function Chargement(listeVille, apikey) {
+    if (listeVille.length !== 0) {
         for (i = 0; i < listeVille.length; i++) {
-            afficherMeteo(listeVille[i], cleApi);
+            afficherMeteo(listeVille[i], apikey);
         }
     }
+    return true
 }
 
-// ajoute une ville
-function ajoutVille() {
-    let nomVille = inputVille.value.trim().toLowerCase();  //permet de mettre le nom rentré dans l'inoput toujours sous la même forme, evite les espaces en trop
+//AJOUT D'UNE VILLE-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// ajoute une ville lors du click du bouton search
+rechercheBtn.addEventListener("click", function () {
+    ajoutVille(barreDeRechercheVille, cleApi)
+});
+
+// ajoute une ville en appuyant sur le bouton "Entrée"
+barreDeRechercheVille.addEventListener('keypress', function (event) {
+    if (event.key === "Enter") {
+        ajoutVille(barreDeRechercheVille, cleApi)
+    }
+})
+
+// Fonction d'ajou d'une ville
+function ajoutVille(inputVille, apikey) {
+
+    let nomVille = inputVille.value.trim().toLowerCase();
 
     if (nomVille !== "") {
-        let existe = listeVille.indexOf(nomVille);
-        if (existe == -1) {
-            afficherMeteo(nomVille, cleApi, true);
+        if (villeDansLocalStorage(nomVille) == false) {
+            afficherMeteo(nomVille, apikey);
+            implementLocalStorage(nomVille);
         }
 
     } else {
         alert('Veuillez saisir le nom d\'une ville');
     }
+
+    return true
 }
+
+//SUPPRESSION DES DONNEES METEO D'UNE VILLE----------------------------------------------------------------------------------------------------------------------------------------
 
 //supprime une ville
-function supVille(event) {
-    supprimerBtn = event.target;
+container.addEventListener('click', prepaSuppressionVille);
+
+//Fonction préparant la suppression en identifiant la ville à supprimer en fonction du bouton cliqué (1 bouton par ligne)
+function prepaSuppressionVille(event) {
+    let supprimerBtn = event.target;
     if (supprimerBtn.className == 'deleteicon') {
-        villeASupprimer = supprimerBtn.parentNode.parentNode.querySelector('.ville').textContent.toLowerCase();
-        villeIndex = listeVille.indexOf(villeASupprimer);
+        let villeASupprimer = supprimerBtn.parentNode.parentNode.querySelector('.ville').textContent.toLowerCase();
 
-        supprimerValidation.parentElement.classList.remove('visible');
-        let deletetext = document.getElementById('supTexte');
-        deletetext.textContent = 'Voulez-vous vraiment supprimer la ville de ' + villeASupprimer + " ?";
-
+        supprimerValidation.classList.remove('visible');
+        deletetext.innerHTML = 'Voulez-vous vraiment supprimer la ville de <span id = "spanvilleasup">' + villeASupprimer + "</span> ?"; //le span permet de stocker le nom de la ville pour pouvoir le récupérer lors de la confirmation
+        return;
     }
+    return
 }
 
-
-
-
+//Action suite au click du bouton "confirmer" de la modale de validation de suppression
 confirmerSupprimer.addEventListener('click', function () {
-    listeVille.splice(villeIndex, 1);
-    localStorage.setItem('ville', JSON.stringify(listeVille));
-    supprimerBtn.parentNode.parentNode.remove();
-    supprimerValidation.parentElement.classList.add('visible');
+    let spanvilleasup = document.getElementById('spanvilleasup');
+    let villeASupprimer = spanvilleasup.textContent;
+    suppressionVille(villeASupprimer);
 })
 
+//Annulation de la suppression
 supprimerAnnule.addEventListener('click', function () {
-    supprimerValidation.parentElement.classList.add('visible');
+    let spanvilleasup = document.getElementById('spanvilleasup');
+    spanvilleasup.textContent = ""; //purge le span du texte de la modale d'annulation de suppression pour éviter une erreur en cas de bug (car il sert de stockage pour la validation de suppression)
+    supprimerValidation.classList.add('visible');
 })
 
+// Fonction qui finalise la suppression et purge le localstorage de la ville supprimée
+function suppressionVille(nomVille) {
+    supprimerDeLocalStorage(nomVille);
 
-
-
-// Affiche les données pour chaque ville stockées dans localstorage au démarrage de l'appli
-document.onload = Chargement(extractLocalStorage());
-
-// ajoute une ville lors du click du bouton search et implémente la liste de ville dans localstorage
-rechercheBtn.addEventListener('click', ajoutVille)
-
-inputVille.addEventListener('keypress', function (event) {
-    if (event.key === "Enter") {
-        ajoutVille()
-    }
-})
-
-// supprime une ligne météo
-
-container.addEventListener('click', supVille)
-
-
-
-
+    let ligneASupprimer = document.getElementById(nomVille);
+    ligneASupprimer.remove();
+    supprimerValidation.classList.add('visible');
+}
 
 
 
